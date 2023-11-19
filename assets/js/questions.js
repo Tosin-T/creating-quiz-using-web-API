1a55504583fbb5bcb868ba31c8a720b720e4e008
tosin-t <tosin100@homail.com>
2023-11-19 21:42:18 +0000

    responsive timer to wrong answer

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -66,6 +66,7 @@ buttonchoices.addEventListener("click",function(event){
             quizchecker()}
         
     }else{ 
+        
         questionPosition++;
         console.log("you suck")}
         if( questionPosition>=quizquestion.length){
@@ -74,5 +75,40 @@ buttonchoices.addEventListener("click",function(event){
         qtitle.innerHTML=''
         buttonchoices.innerHTML=''
             quizchecker()}
+            timeleft-=10
     })
     
+// // declared variables
+var startscreen= document.getElementById("start-screen")
+var startB=document.getElementById('start')
+var q= document.getElementById("questions")
+
+console.log(q)
+///// timer///
+var timerdisplay=document.getElementById("time")
+var timeleft= 100
+
+function initiatetimer(){timerFunction= setInterval(function(){
+  
+  timeleft--
+  timerdisplay.innerText=timeleft
+
+
+  if(timeleft==0){
+    clearInterval(timerFunction)
+
+  }
+},1000)}
+
+function changevisibility(){
+  q.className="visible"
+
+}
+function changevisibility2(){
+  startscreen.style.visibility= 'hidden'  
+
+}
+// called functions
+startB.addEventListener("click",initiatetimer)
+startB.addEventListener("click",changevisibility)
+startB.addEventListener("click",changevisibility2)
